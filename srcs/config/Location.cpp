16f5a76438c991ac66601c00214dbd7cf8333e0a
eschwart <eschwart@student.42.fr>
2025-12-17 13:45:15 +0100

    first push class Location

--- srcs/config/Location.cpp
+++ srcs/config/Location.cpp
@@ -6,16 +6,137 @@
 /*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:20:46 by eschwart          #+#    #+#             */
-/*   Updated: 2025/12/16 13:48:35 by eschwart         ###   ########.fr       */
+/*   Updated: 2025/12/17 13:44:00 by eschwart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// =============================================================================
+// Includes
+
 #include "Location.hpp"
 
-Location::Location()
+// =============================================================================
+// Handlers
+
+// Default constructor
+Location::Location() :
+	_path(""),
+	_root(""),
+	_autoIndex(false),
+	_uploadPath(""),
+	_redirect(""),
+	_cgiExtension(""),
+	_cgiPath("")
+{
+}
+// Destructor
+Location::~Location()
 {
 }
 
-Location::~Location()
+// =============================================================================
+// Setters
+
+void Location::setPath(const std::string &path)
 {
+	_path = path;
+}
+
+void Location::setRoot(const std::string &root)
+{
+	_root = root;
+}
+
+void Location::addAllowedMethod(const std::string &method)
+{
+	_allowedMethods.push_back(method);
+}
+
+void Location::addIndex(const std::string &index)
+{
+	_index.push_back(index);
+}
+
+void Location::setAutoIndex(bool autoIndex)
+{
+	_autoIndex = autoIndex;
+}
+
+void Location::setUploadPath(const std::string &path)
+{
+	_uploadPath = path;
+}
+
+void Location::setRedirect(const std::string &redirect)
+{
+	_redirect = redirect;
+}
+
+void Location::setCgiExtension(const std::string &ext)
+{
+	_cgiExtension = ext;
+}
+
+void Location::setCgiPath(const std::string &path)
+{
+	_cgiPath = path;
+}
+
+// =============================================================================
+// Getters
+
+const std::string &Location::getPath() const
+{
+	return _path;
+}
+
+const std::string &Location::getRoot() const
+{
+	return _root;
+}
+
+const std::vector<std::string> &Location::getAllowedMethods() const
+{
+	return _allowedMethods;
+}
+
+const std::vector<std::string> &Location::getIndex() const
+{
+	return _index;
+}
+
+bool Location::getAutoIndex() const
+{
+	return _autoIndex;
+}
+
+const std::string &Location::getUploadPath() const
+{
+	return _uploadPath;
+}
+
+const std::string &Location::getRedirect() const
+{
+	return _redirect;
+}
+
+const std::string &Location::getCgiExtension() const
+{
+	return _cgiExtension;
+}
+
+const std::string &Location::getCgiPath() const
+{
+	return _cgiPath;
+}
+
+// =============================================================================
+// Methods
+
+bool Location::isMethodAllowed(const std::string &method) const
+{
+	for (size_t i = 0; i < _allowedMethods.size(); i++)
+		if (_allowedMethods[i] == method)
+			return true;
+	return false;
 }
--- srcs/config/Location.hpp
+++ srcs/config/Location.hpp
@@ -6,7 +6,7 @@
 /*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:20:55 by eschwart          #+#    #+#             */
-/*   Updated: 2025/12/16 13:50:06 by eschwart         ###   ########.fr       */
+/*   Updated: 2025/12/17 13:44:14 by eschwart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,90 @@
 // Upload path
 // Redirection
 
+// =============================================================================
+// Includes
+
 #pragma once
 
 #include <string>
 #include <vector>
 
+// =============================================================================
+// Typedef
+
+
+// =============================================================================
+// Defines
+
+
+// =============================================================================
+// Class Location
+
+
 class Location
 {
 private:
-	// TODO: std::string _path;
-	// TODO: std::string _root;
-	// TODO: std::vector<std::string> _allowedMethods;
-	// TODO: std::vector<std::string> _index;
-	// TODO: bool _autoindex;
-	// TODO: std::string _uploadPath;
-	// TODO: std::string _redirect;
-	// TODO: std::string _cgiExtension;
-	// TODO: std::string _cgiPath;
+	// =========================================================================
+	// Attributs
+
+	// "/", "/uploads", etc.
+	std::string _path;
+
+	// "./wwww"
+	std::string _root;
+
+	// ["GET", "POST"]
+	std::vector<std::string> _allowedMethods;
+
+	// ["index.html"]
+	std::vector<std::string> _index;
+
+	// true/false
+	bool _autoIndex;
+
+	// "./uploads"
+	std::string _uploadPath;
+
+	// "" or URL redirection
+	std::string _redirect;
+
+	// ".php"
+	std::string _cgiExtension;
+
+	// "usr/bin/php-cgi"
+	std::string _cgiPath;
 
 public:
+	// =========================================================================
+	// Handlers
 	Location();
 	~Location();
 
-	// TODO: Getters/Setters
-	// TODO: bool isMethodAllowed(const std::string &method) const;
+	// =========================================================================
+	// Setters => set replace val, add add val
+	void setPath(const std::string &path);
+	void setRoot(const std::string &root);
+	void addAllowedMethod(const std::string &method);
+	void addIndex(const std::string &index);
+	void setAutoIndex(bool autoIndex);
+	void setUploadPath(const std::string &path);
+	void setRedirect(const std::string &redirect);
+	void setCgiExtension(const std::string &ext);
+	void setCgiPath(const std::string &path);
+
+	// =========================================================================
+	// Getters
+	const std::string &getPath() const;
+	const std::string &getRoot() const;
+	const std::vector<std::string> &getAllowedMethods() const;
+	const std::vector<std::string> &getIndex() const;
+	bool getAutoIndex() const;
+	const std::string &getUploadPath() const;
+	const std::string &getRedirect() const;
+	const std::string &getCgiExtension() const;
+	const std::string &getCgiPath() const;
+
+	// =========================================================================
+	// Methods
+	bool isMethodAllowed(const std::string &method) const;
 };
