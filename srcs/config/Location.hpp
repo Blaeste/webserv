48ca8b373ab2d29183d2057b199d464e1aea08ca
eschwart <eschwart@student.42.fr>
2025-12-18 09:51:14 +0100

    patch utils docstring + add ServerConfig docstring

--- srcs/config/Location.hpp
+++ srcs/config/Location.hpp
@@ -6,7 +6,7 @@
 /*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:20:55 by eschwart          #+#    #+#             */
-/*   Updated: 2025/12/17 14:01:34 by eschwart         ###   ########.fr       */
+/*   Updated: 2025/12/18 09:49:55 by eschwart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@
 
 // =============================================================================
 // Class Location
-
 
 class Location
 {
@@ -75,34 +74,134 @@ private:
 public:
 	// =========================================================================
 	// Handlers
+
+	// Default constructor
 	Location();
+
+	// Destructor
 	~Location();
 
 	// =========================================================================
 	// Setters => set replace val, add add val
+
+	/**
+	 * @brief Sets the path of the location.
+	 * @param path The path to set (e.g., "/uploads").
+	 */
 	void setPath(const std::string &path);
+
+	/**
+	 * @brief Sets the root directory for this location.
+	 * @param root The root directory path.
+	 */
 	void setRoot(const std::string &root);
+
+	/**
+	 * @brief Adds an allowed HTTP method to the location.
+	 * @param method The HTTP method to allow (e.g., "GET", "POST").
+	 */
 	void addAllowedMethod(const std::string &method);
+
+	/**
+	 * @brief Adds an index file to the location.
+	 * @param index The name of the index file to add.
+	 */
 	void addIndex(const std::string &index);
+
+	/**
+	 * @brief Sets the auto-indexing option.
+	 * @param autoIndex true to enable auto-indexing, false to disable.
+	 */
 	void setAutoIndex(bool autoIndex);
+
+	/**
+	 * @brief Sets the upload path.
+	 * @param path The upload path.
+	 */
 	void setUploadPath(const std::string &path);
+
+	/**
+	 * @brief Sets the redirection URL.
+	 * @param redirect The redirection URL.
+	 */
 	void setRedirect(const std::string &redirect);
+
+	/**
+	 * @brief Sets the CGI file extension.
+	 * @param ext The CGI file extension (e.g., ".php").
+	 */
 	void setCgiExtension(const std::string &ext);
+
+	/**
+	 * @brief Sets the CGI executable path.
+	 * @param path The path to the CGI executable.
+	 */
 	void setCgiPath(const std::string &path);
 
 	// =========================================================================
 	// Getters
+
+	/**
+	 * @brief Gets the path of the location.
+	 * @return The path as a string.
+	 */
 	const std::string &getPath() const;
+
+	/**
+	 * @brief Gets the root directory.
+	 * @return The root directory as a string.
+	 */
 	const std::string &getRoot() const;
+
+	/**
+	 * @brief Gets the list of allowed HTTP methods.
+	 * @return A constant reference to a vector of allowed methods.
+	 */
 	const std::vector<std::string> &getAllowedMethods() const;
+
+	/**
+	 * @brief Gets the list of index files.
+	 * @return A constant reference to a vector of index file names.
+	 */
 	const std::vector<std::string> &getIndex() const;
+
+	/**
+	 * @brief Gets the auto-indexing setting.
+	 * @return true if auto-indexing is enabled, false otherwise.
+	 */
 	bool getAutoIndex() const;
+
+	/**
+	 * @brief Gets the upload path.
+	 * @return The upload path as a string.
+	 */
 	const std::string &getUploadPath() const;
+
+	/**
+	 * @brief Gets the redirection URL.
+	 * @return The redirection URL as a string.
+	 */
 	const std::string &getRedirect() const;
+
+	/**
+	 * @brief Gets the CGI file extension.
+	 * @return The CGI file extension as a string.
+	 */
 	const std::string &getCgiExtension() const;
+
+	/**
+	 * @brief Gets the CGI executable path.
+	 * @return The CGI executable path as a string.
+	 */
 	const std::string &getCgiPath() const;
 
 	// =========================================================================
 	// Methods
+
+	/**
+	 * @brief Checks if a given HTTP method is allowed in this location.
+	 * @param method The HTTP method to check (e.g., "GET", "POST").
+	 * @return true if the method is allowed, false otherwise.
+	 */
 	bool isMethodAllowed(const std::string &method) const;
 };
--- srcs/utils/utils.hpp
+++ srcs/utils/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: eschwart <eschwart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:22:52 by eschwart          #+#    #+#             */
-/*   Updated: 2025/12/18 09:43:18 by eschwart         ###   ########.fr       */
+/*   Updated: 2025/12/18 09:50:44 by eschwart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@
 
 /**
  * @brief Utility functions for string manipulation, file handling, and date formatting.
- *
  * @param str The input string to trim.
  * @return A new string with leading and trailing whitespace removed.
  */
@@ -31,7 +30,6 @@ std::string trim(const std::string &str);
 
 /**
  * @brief Splits a string into a vector of substrings based on a delimiter.
- *
  * @param str The input string to split.
  * @param delimiter The character used as the delimiter.
  * @return A vector containing the split substrings.
@@ -40,7 +38,6 @@ std::vector<std::string> split(const std::string &str, char delimiter);
 
 /**
  * @brief Checks if a character is a hexadecimal digit.
- *
  * @param c The character to check.
  * @return true if the character is a hex digit, false otherwise.
  */
@@ -48,7 +45,6 @@ bool isHexDigit(char c);
 
 /**
  * @brief Decodes a URL-encoded string.
- *
  * @param url The URL-encoded string.
  * @return The decoded string.
  */
@@ -56,7 +52,6 @@ std::string urlDecode(const std::string &url);
 
 /**
  * @brief Checks if a file exists at the given path.
- *
  * @param path The file path to check.
  * @return true if the file exists, false otherwise.
  */
@@ -64,7 +59,6 @@ bool fileExists(const std::string &path);
 
 /**
  * @brief Checks if the given path is a directory.
- *
  * @param path The path to check.
  * @return true if the path is a directory, false otherwise.
  */
@@ -72,7 +66,6 @@ bool isDirectory(const std::string &path);
 
 /**
  * @brief Retrieves the file extension from a given file path.
- *
  * @param path The file path.
  * @return The file extension, including the dot (e.g., ".txt"), or an empty string if none exists.
  */
@@ -80,14 +73,12 @@ std::string getFileExtension(const std::string &path);
 
 /**
  * @brief Gets the current date and time formatted for HTTP headers.
- *
  * @return A string representing the current date and time in HTTP format.
  */
 std::string getHttpDate();
 
 /**
  * @brief Reads the entire content of a file into a string.
- *
  * @param path The path to the file.
  * @return A string containing the file's content.
  */
@@ -95,7 +86,6 @@ std::string readFile(const std::string &path);
 
 /**
  * @brief Gets the size of a file in bytes.
- *
  * @param path The path to the file.
  * @return The size of the file in bytes, or 0 if the file does not exist or is not a regular file.
  */
