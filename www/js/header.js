256daf1d4b65b7bfec81d5a2f6c329dfb23d132b
eschwart <eschwart@student.42.fr>
2026-01-06 10:23:12 +0100

    Cleaning + add page test for php

--- www/js/header.js
+++ www/js/header.js
@@ -5,6 +5,7 @@ function loadHeader() {
 				<a href="/">Home</a>
 				<a href="/files.html">Files</a>
 				<a href="/about.html">About</a>
+				<a href="/cgi-bin/php/qrcode.php">QR Code</a>
 				<a href="/contact.html">Contact</a>
 			</nav>
 		</header>
