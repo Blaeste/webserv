f59b65afddf206d776b1955efdf8536837225a2b
Gregory Dosch <gdosch@k1r3p16.42mulhouse.fr>
2026-03-10 11:14:23 +0100

    refactor: drop errno checks in numeric parsing routines

--- srcs/utils/utils.cpp
+++ srcs/utils/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: gdosch <gdosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 10:22:49 by eschwart          #+#    #+#             */
-/*   Updated: 2026/03/10 11:11:12 by gdosch           ###   ########.fr       */
+/*   Updated: 2026/03/10 11:14:06 by gdosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <sstream>		// std::stringstream
 #include <stdexcept>	// std::runtime_error
 #include <cctype>		// std::tolower
-#include <cerrno>		// errno, ERANGE
+#include <cerrno>		// errno
 #include <cstdlib>		// std::strtol
 #include <cstring>		// std::strerror
 #include <fcntl.h>		// open, fcntl, O_RDONLY, O_NONBLOCK, F_GETFL, F_SETFL
